c56bebaa52bdca4dd570f4cb5a104060cf7258ab
sportsbar-prog <sportsbar@lynbrookhotel.com.au>
2025-09-16 17:46:31 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
+# Twilio Python SDK - Package Init
+# This is the main entry point for the Twilio package
 
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioException, TwilioRestException
+
+__version__ = "1.0.0"
+__all__ = ["Client", "TwilioException", "TwilioRestException"]
